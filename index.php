2223c40e20221b79a60c249a962c1f17cb21dc42
qinee <qineefu@gmail.com>
2026-03-17 08:36:29 +0800

    Drafted a index page

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html> 
+<html> 
+    <head> 
+        <title>Internship Result system </title> 
+    </head>
+    <body>
+        <h1>Internship Result Management System </h1> 
+        <a href ="login.php">Login </a> 
+</body>
+</html> 
